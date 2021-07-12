8cf0a644a7c0e5ffcbc15d55fc63bca021e5138c
Renato Zangerolami <renato.zanger@gmail.com>
2021-07-12 09:40:34 -0300

    primeiro commit

---
+++ personapi/src/test/java/org/digitalinnovation/personapi/PersonapiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.digitalinnovation.personapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
