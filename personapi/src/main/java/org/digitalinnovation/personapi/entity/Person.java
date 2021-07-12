bdf7135917beba591348983e62db2435126ef382
Renato Zangerolami <renatinhoz@hotmail.com>
2021-07-12 17:28:38 -0300

    Pacotes da entidades e enum adicionados

---
+++ personapi/src/main/java/org/digitalinnovation/personapi/entity/Person.java
@@ -0,0 +1,39 @@
+package org.digitalinnovation.personapi.entity;
+
+import io.micrometer.core.instrument.internal.Mergeable;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.digitalinnovation.personapi.enums.PhoneType;
+import org.springframework.lang.Nullable;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String fisrtName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false,unique = true)
+    private String cpf;
+
+    private LocalDate birthDate;
+
+    @OneToMany(fetch = FetchType.LAZY,cascade = {CascadeType.PERSIST, CascadeType.MERGE,CascadeType.REMOVE})
+    private List<Phone> phones;
+}
